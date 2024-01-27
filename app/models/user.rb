64a8f879e2223746893cababe6077fe100ce1ae4
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-01-27 16:19:09 +0530

    Add Comment model with associations
    - Created the Comment model with necessary attributes.
    - Established associations with the User and Post models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :post
+  has_many :comments
   validates :username, length: { in: 4..20 }, uniqueness: true
   validates :email,uniqueness: true, presence: true
 end
